8a80ec623a152366f2858a9c8d3db60e89fb539c
aditya2337 <aditya.n@gai.co.in>
2017-08-31 17:45:17 +0530

    adding of song via mutation done

---
+++ lyricalGraphQL/client/components/SongCreate.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: ''
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create a new song</h3>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label htmlFor="input">Song Title:</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ title: e.target.value })}
+            value={this.state.title}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
